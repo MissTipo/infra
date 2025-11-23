026c3cebe6df31f5ccb94791b073cb3944a08ff6
MissTipo <dorine.a.tipo@gmail.com>
2025-11-23 18:28:59 +0300

    feat(network): define input variables for network module
    Introduce configurable variables for project name, environment, region,
    compartment OCID, tenancy, user, fingerprint, private key path, and other network parameters.
    Enables reuse and environment-specific configuration.
    
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

---
+++ modules/network/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "project" {}
+variable "compartment_ocid" {}
+variable "environment" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
