486cd40e1b4683563e0917e1374a8af52c3a90a9
MissTipo <dorine.a.tipo@gmail.com>
2025-11-27 12:03:35 +0300

    chore(compute): define variables.tf for compute module
    Declared all required and optional input variables for the compute module.
    This includes OCI credentials, tenancy, compartment, shape, subnet, availability domain,
    SSH keys, image OCID, and instance naming inputs. Default values and descriptions
    added where applicable to improve usability and clarity.
    
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,5 +1,39 @@
 variable "region" {}
 variable "project" {}
-variable "account_id" {}
 variable "environment" {}
+variable "compartment_ocid" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
+# variable "image_ocid" {}
+
+variable "availability_domain" {
+  description = "OCI availability domain (e.g. Uocm:SA-JOHANNESBURG-1-AD-1)"
+}
+
+variable "shape" {
+  description = "Compute shape"
+  default     = "VM.Standard.E2.1.Micro"
+  # default     = "Canonical-Ubuntu-24.04-2025.10.31-0"
+}
+
+variable "subnet_ocid" {
+  description = "Subnet OCID for the VM"
+}
+
+variable "assign_public_ip" {
+  description = "Whether to give the instance a public IP"
+  type        = bool
+  default     = false
+}
+
+variable "ssh_public_key" {
+  description = "Path to your SSH public key"
+}
+
+variable "image_ocid" {
+  description = "OCID of the image to use for the instance"
+  type        = string
+}
 
