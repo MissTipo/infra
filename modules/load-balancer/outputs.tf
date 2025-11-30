ed185725896aee58dcb26b54fbfa62acb1d28068
MissTipo <dorine.a.tipo@gmail.com>
2025-11-30 16:34:10 +0300

    chore(lb): expose outputs for downstream use
    Add outputs for load balancer IP, listener names, and backend set names to allow
    other modules or scripts to reference LB endpoints easily.
    
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- modules/load-balancer/outputs.tf
+++ modules/load-balancer/outputs.tf
@@ -1,5 +1,17 @@
-# Placeholder outputs
-output "module_ready" {
-  value = true
+output "load_balancer_public_ip" {
+  value = oci_load_balancer_load_balancer.lb.ip_address_details[0].ip_address
 }
+
+output "load_balancer_id" {
+  value = oci_load_balancer_load_balancer.lb.id
+}
+
+output "listener" {
+  value = oci_load_balancer_listener.listener.name
+}
+
+output "backend_set" {
+  value = oci_load_balancer_backend_set.backend_set.name
+}
+
 
