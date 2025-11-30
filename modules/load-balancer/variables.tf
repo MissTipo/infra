314e9a88721a6b0ef83631940f9365275c911024
MissTipo <dorine.a.tipo@gmail.com>
2025-11-30 16:33:53 +0300

    chore(lb): define required input variables
    Add variables for compartment, subnet IDs, backend IPs, ports, listener names and ports
    so the module can be reused across environments without interactive input.
    
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- modules/load-balancer/variables.tf
+++ modules/load-balancer/variables.tf
@@ -1,5 +1,58 @@
-variable "region" {}
-variable "project" {}
-variable "account_id" {}
-variable "environment" {}
+variable "region" {
+  type = string
+}
+
+variable "compartment_ocid" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "lb_name" {
+  type = string
+}
+
+variable "is_private" {
+  type    = bool
+  default = false
+}
+
+variable "backend_set_name" {
+  type = string
+}
+
+variable "backend_ip" {
+  type = string
+}
+
+variable "backend_port" {
+  type    = number
+  default = 80
+}
+
+variable "listener_name" {
+  type = string
+}
+
+variable "listener_port" {
+  type    = number
+  default = 80
+}
+
+variable "backend_sets" {
+  type = list(object({
+    name           = string
+    policy         = string
+    backends       = list(object({ ip_address = string, port = number }))
+    health_checker = object({
+      protocol    = string
+      url_path    = string
+      port       = number
+      retries    = number
+      timeout_in_seconds = number
+    })
+  }))
+}
 
